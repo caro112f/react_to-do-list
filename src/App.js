f68191e6ea73ae26cca1287915cf83e393973691
Caroline Nielsen <caro112f@stud.kea.dk>
2022-04-25 19:20:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import ListItem from "./components/ListItem";
+
+import { useState } from "react";
+
+const item = {
+  id: 1,
+  task: "do dishes",
+  completed: false,
+};
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      task: "clean stuff",
+      completed: false,
+    },
+    {
+      id: 2,
+      task: "wash",
+      completed: true,
+    },
+  ]);
+
+  const [showCompleted, setShowCompleted] = useState(true);
+
+  let all = tasks;
+  if (!showCompleted) {
+    all = tasks.filter((item) => item.completed === false);
+  }
+
+  function deleteItem(id) {
+    setTasks((oldState) => oldState.filter((item) => item.id !== id));
+  }
+  function submit(e) {
+    e.preventDefault();
+
+    const newTask = {
+      id: Math.random(),
+      task: e.target.elements.name.value,
+      completed: false,
+    };
+    setTasks((oldState) => oldState.concat(newTask));
+  }
+
+  function toggleCompletion(id) {
+    setTasks((oldState) => {
+      return oldState.map((item) => {
+        if (id === item.id) {
+          const newItem = { ...item };
+          newItem.completed = !newItem.completed;
+          return newItem;
+        }
+        return item;
+      });
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <button
+        onClick={() => {
+          setShowCompleted((oldState) => !oldState);
+        }}
+      >
+        Remove completed tasks
+      </button>
+      <form onSubmit={submit}>
+        <label htmlFor="name">Task</label>
+        <input type="text" id="name" name="task" />
+        <button>Add task</button>
+      </form>
+      <ul>
+        {all.map((task) => (
+          <ListItem
+            toggleCompletion={toggleCompletion}
+            deleteItem={deleteItem}
+            {...task}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/components/ListItem.js
@@ -0,0 +1,17 @@
+export default function ListItem(props) {
+  function handleDelete() {
+    props.deleteItem(props.id);
+  }
+  function handleCompletion() {
+    props.toggleCompletion(props.id);
+  }
+  return (
+    <li className={props.completed ? "completed" : ""}>
+      {props.task}{" "}
+      <button onClick={handleCompletion}>
+        {props.completed ? "Undo" : "Complete"}
+      </button>
+      <button onClick={handleDelete}>Delete</button>
+    </li>
+  );
+}
